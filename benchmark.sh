48dd3d5087d97a966cae883b070182a4c9a1a70f
Andrei Shevchuk <andrei@shevchuk.co>
2023-10-10 21:26:35 +0300

    -

--- benchmark.sh
+++ benchmark.sh
@@ -3,7 +3,8 @@
 export DEBIAN_FRONTEND=noninteractive
 
 results=results.txt
-DEPS=(build-essential ca-certificates devscripts autoconf autoconf-archive automake bison cmake doxygen)
+#DEPS=(build-essential ca-certificates devscripts autoconf autoconf-archive automake bison cmake doxygen)
+DEPS=(build-essential ca-certificates devscripts)
 
 _clean_pkgs() {
   sudo apt purge -y "${DEPS[@]}"
@@ -18,7 +19,7 @@ _install_pkgs() {
 _run_tests() {
   booster=${1:-}
   T0=$(date +%s)
-  for i in {1..5}; do
+  for i in {1..2}; do
     t0=$(date +%s)
     _install_pkgs $booster
     echo ${booster:-regular}: $(( $(date +%s) - t0 )) s | tee -a "$results"
@@ -28,12 +29,15 @@ _run_tests() {
   echo Total ${booster:-regular}: $(( $(date +%s) - T0 )) s | tee -a "$results"
 }
 
+sudo mkdir -p /etc/apt/apt.conf.d
+echo 'Binary::apt::APT::Keep-Downloaded-Packages "1";' | sudo tee /etc/apt/apt.conf.d/10apt-keep-downloads
+
 sudo apt update
 _clean_pkgs
 sudo apt install --download-only -y "${DEPS[@]}"
 #sync; sysctl -q vm.drop_caches=3
 
-_run_tests
+_run_tests ""
 _run_tests eatmydata
 
 cat $results
